a301c1bd9b5bff919c4d12ef91be3a011b1bc889
Todd Stout <btstout@containerstore.com>
2020-04-16 07:31:25 -0500

    bump version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.tstout/spring-repl "0.1.0"
+(defproject com.github.tstout/spring-repl "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
